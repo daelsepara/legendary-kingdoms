a279b0b6bc1602f38a25534511fabe1d1ae61705
Dael Separa <dael.separa@gmail.com>
2021-08-22 23:34:56 +0800

    switch next and preview

--- src/story.hpp
+++ src/story.hpp
@@ -1088,8 +1088,8 @@ namespace Story
         controls.push_back(Button(idx + 1, "icons/map.png", idx, idx + 2, compact ? idx + 1 : 1, idx + 1, startx + gridsize, buttony, Control::Type::MAP));
         controls.push_back(Button(idx + 2, "icons/disk.png", idx + 1, idx + 3, compact ? idx + 2 : 1, idx + 2, startx + 2 * gridsize, buttony, Control::Type::GAME));
         controls.push_back(Button(idx + 3, "icons/user.png", idx + 2, idx + 4, compact ? idx + 3 : 1, idx + 3, startx + 3 * gridsize, buttony, Control::Type::PARTY));
-        controls.push_back(Button(idx + 4, "icons/next.png", idx + 3, idx + 5, compact ? idx + 4 : 1, idx + 4, startx + 4 * gridsize, buttony, Control::Type::NEXT));
-        controls.push_back(Button(idx + 5, "icons/swords.png", idx + 4, idx + 6, compact ? idx + 5 : 1, idx + 5, startx + 5 * gridsize, buttony, Control::Type::PREVIEW));
+        controls.push_back(Button(idx + 4, "icons/swords.png", idx + 3, idx + 5, compact ? idx + 4 : 1, idx + 4, startx + 4 * gridsize, buttony, Control::Type::PREVIEW));
+        controls.push_back(Button(idx + 5, "icons/next.png", idx + 4, idx + 6, compact ? idx + 5 : 1, idx + 5, startx + 5 * gridsize, buttony, Control::Type::NEXT));
         controls.push_back(Button(idx + 6, "icons/exit.png", idx + 5, idx + 6, compact ? idx + 6 : 1, idx + 6, (1.0 - Margin) * SCREEN_WIDTH - buttonw, buttony, Control::Type::BACK));
 
         return controls;
